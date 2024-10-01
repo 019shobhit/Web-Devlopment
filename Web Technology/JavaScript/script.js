a8b3959606157fd8236a8077df01aadb743c1e7d
019shobhit <019shobhit@gmail.com>
2024-10-01 10:25:57 +0530

    pus all new codes

---
+++ Web Technology/JavaScript/script.js
@@ -0,0 +1,40 @@
+// console.log("hello world 123");
+
+// VAR
+// var a = 20;
+// var b = 60;
+// var name = "shobhit";
+// console.log("the sum of a and b is: "+(a+b));
+// console.log(name);
+
+// LET
+// let  x = 10;
+// let y = 20;
+// console.log(x+y);
+
+// // access 
+// let n = 10;
+// {
+//     var m = 15;
+//     console.log(n+m);
+// }
+// console.log(n+m);
+
+// type of
+// let a = "shobhit";
+// let b = true;
+// let c ;
+// let d = null;
+// let e = 'v';
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+// console.log(typeof(e));
+
+// symbol
+// let a =  Symbol(10);
+// let b =  Symbol(10);
+// console.log();
+
+// OBJECT
