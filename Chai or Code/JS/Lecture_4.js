51eb0370703c35e05767d4abaae0dd2f8c95f802
019shobhit <019shobhit@gmail.com>
2025-01-27 23:32:56 +0530

    add

--- Chai or Code/JS/Lecture_4.js
+++ Chai or Code/JS/Lecture_4.js
@@ -2,3 +2,16 @@ const accountId = 1233;
 let accountEmail = "shobhitchauhan2002@gmail.com";
 var accountPassword = "shobhit2002";
 accountCity = "baghpat";
+
+// accountId = 2
+accountEmail = "019shobhit"
+accountPassword = "vanshi"
+accountCity = "shamli"
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+
+
