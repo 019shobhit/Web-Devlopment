be1c3bc62cf9721b02c66be037d68835e75000b0
019shobhit <019shobhit@gmail.com>
2025-02-07 15:44:37 +0530

    add new files

---
+++ Chai or Code/05_dataTypes.js
@@ -0,0 +1,8 @@
+let name = "shobhit chauhan"
+let age = 22;
+let ready = "almost";
+let x = 10;
+console.log(x);
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof(ready));
---
+++ Chai or Code/JS/04_Variables.js
@@ -0,0 +1,11 @@
+// console.log("shobhit")
+const c = "Constant keyword"
+console.log(c)
+
+let name = "chauhan"
+var kam = "fixed"
+console.table([name,kam])
+
+name = "shobhit"
+kam = "hai hi nahi"
+console.table([name,kam])
