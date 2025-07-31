cbd713099ba19d7effb2078415269fe05561d6a8
019shobhit@gmail.com <019shobhit@gmail.com>
2025-07-31 14:14:02 +0530

    add new files

--- code with harry/JavaScript/22_Exercise_1/script.js
+++ code with harry/JavaScript/22_Exercise_1/script.js
@@ -1,1 +1,9 @@
-// Guess the number
+// Guess the number Exercise
+
+let number = 10;
+let count = 0;
+while(number != 0){
+    number--;
+    count++;
+}
+console.log("you gess in :",count,"numbers")
