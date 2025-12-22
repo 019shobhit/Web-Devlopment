1778e0393fab4c1ed0041ee7fe5f2641b62e5188
019shobhit@gmail.com <019shobhit@gmail.com>
2025-12-22 14:12:02 +0530

    add new files

---
+++ Coding School/02-folder/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+    return (
+        <div className='card1'>
+            <h1>This is my first card</h1>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex corrupti ratione voluptatem quia asperiores voluptatum qui rem illum nihil modi?</p>
+        </div>
+    )
+}
+
+export default Card;
