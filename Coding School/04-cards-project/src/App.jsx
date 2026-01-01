b78d363c70da466454c1cac77005366cb6e1c585
019shobhit@gmail.com <019shobhit@gmail.com>
2026-01-02 00:04:05 +0530

    add new files

--- Coding School/04-cards-project/src/App.jsx
+++ Coding School/04-cards-project/src/App.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import Cards from './components/Cards'
-
 const App = () => {
+  const arr = [
+    {
+      user: 'shobhit',
+      age: 24
+    },
+    {
+      user: 'john',
+      age: 30
+    }, {
+      user: 'doe',
+      age: 22
+    },
+    {
+      user: 'smith',
+      age: 28
+    }
+  ];
+
+  arr.map(function (elem) {
+    console.log(elem.user);
+  })
   return (
     <div className='parent' >
-      <Cards />
+
     </div>
   )
 }
