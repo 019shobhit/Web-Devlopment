e79367b2e58515e575395b49797841354a4d7140
019shobhit@gmail.com <019shobhit@gmail.com>
2026-01-31 19:40:32 +0530

    add new files

---
+++ Coding School/07-ui-project/src/components/section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='flex '>
+        <h4>Target Audience</h4>
+        <button>digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default NavBar
---
+++ Coding School/07-ui-project/src/components/section1/Section1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const Section1 = () => {
+  return (
+    <div className='h-screen w-full bg-blue-500'>
+      Section 1
+      <NavBar/>
+    </div>
+  )
+}
+
+export default Section1
